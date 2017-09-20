ff539743b0f237cfd8df21fbcb76e3ba8c9946ca
Vlad Petyuk <petyuk@gmail.com>
2017-09-20 13:47:11 -0700

    bumping up a number of bioconductor packages

--- init.R
+++ init.R
@@ -32,13 +32,25 @@ install.packages(pkgs)
 
 library(devtools)
 # vp.misc is needed for data ML analysis
-# install_github("vladpetyuk/vp.misc", ref = "d4ce169eb1459a4951c3824187eb5480c1bbae72") # Nov 14, 2016
-install_github("vladpetyuk/vp.misc", ref = "c091e94743f9b5a381936f4fe705cfb1072e7970") # Aug 2, 2016
+install_github("vladpetyuk/vp.misc", ref = "d4ce169eb1459a4951c3824187eb5480c1bbae72") # Nov 14, 2016
+# install_github("vladpetyuk/vp.misc", ref = "c091e94743f9b5a381936f4fe705cfb1072e7970") # Aug 2, 2016
 
 # bump up knitr and rmarkdown
 install_version("knitr", version = "1.14", repos = "http://cran.us.r-project.org")
 install_version("rmarkdown", version = "1.2", repos = "http://cran.us.r-project.org")
 
 # bump BiocStyle
-packageurl <- "https://bioconductor.org/packages/3.4/bioc/src/contrib/BiocStyle_2.2.1.tar.gz"
-install.packages(packageurl, repos=NULL, type="source")
+# packageurl <- "https://bioconductor.org/packages/3.4/bioc/src/contrib/BiocStyle_2.2.1.tar.gz"
+# install.packages(packageurl, repos=NULL, type="source")
+# https://bioconductor.org/packages/3.4/data/annotation/src/contrib/GO.db_3.4.0.tar.gz
+# https://bioconductor.org/packages/3.4/bioc/src/contrib/clusterProfiler_3.2.14.tar.gz
+# https://bioconductor.org/packages/3.4/bioc/src/contrib/ReactomePA_1.18.1.tar.gz
+
+# bump a few Bioconductor packages
+install_url("https://bioconductor.org/packages/3.4/bioc/src/contrib/BiocStyle_2.2.1.tar.gz")
+install_url("https://bioconductor.org/packages/3.4/data/annotation/src/contrib/GO.db_3.4.0.tar.gz")
+install_url("https://bioconductor.org/packages/3.4/bioc/src/contrib/clusterProfiler_3.2.14.tar.gz")
+install_url("https://bioconductor.org/packages/3.4/bioc/src/contrib/ReactomePA_1.18.1.tar.gz")
+
+
+
