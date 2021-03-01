0bb1e10be0f50075e2cd112b35baf192484bea61
Vlad Petyuk <petyuk@gmail.com>
2021-02-28 23:33:08 -0800

    support of the open search piece in the manuscript

--- init.R
+++ init.R
@@ -25,7 +25,8 @@ pkgs <- c("e1071", "doRNG", "caret", "BiocStyle", "RColorBrewer", "knitr",
 	"GOSemSim","DOSE","ReactomePA", "RODBC",
 	"clusterProfiler","FField","org.Hs.eg.db","org.Mm.eg.db",
 	"AnnotationDbi", "impute", "GO.db", "preprocessCore", "WGCNA",
-	"GOstats", "gelnet", "biomaRt","bnlearn","Rgraphviz","gelnet","multcomp")
+	"GOstats", "gelnet", "biomaRt","bnlearn","Rgraphviz","gelnet","multcomp",
+	"pheatmap","dendsort","msmsTests")
 
 install.packages(pkgs)
 
